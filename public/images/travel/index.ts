6b13d88ca8a1d302ac142f2a7d36f21cffc0f932
zhayabin <80046768+zhayabin@users.noreply.github.com>
2024-09-29 02:23:25 +0800

    Update index.ts

--- public/images/travel/index.ts
+++ public/images/travel/index.ts
@@ -1,9 +1,22 @@
-import bura from './bura.jpg';
-import antelope from './antelope.jpg';
-import croatia2 from './croatia2.jpg';
+import 1火车 from './1火车.jpg';
+import 2勐麻河 from './2勐麻河.jpg';
+import 3勐麻河交汇 from './3勐麻河交汇.JPG';
+import 4树上采摘 from './4树上采摘.JPG';
+import 6吊在茶树上 from './6吊在茶树上.jpg';
+import 7勐麻河土基房 from './7勐麻河土基房.jpg';
+import 8在勐麻河竹林 from './8在勐麻河竹林.JPG';
+import 9茶树和我 from './9茶树和我.JPG';
+import 10河边休息 from './10河边休息.png';
 
 export const travelImages = [
-  { img: bura, title: 'Horseshoe Bend, USA', alt: 'Picture taken in Horseshoe, USA' },
-  { img: antelope, title: 'Antelope Canyon, USA', alt: 'Picture taken in Antelope Canyon, USA' },
-  { img: croatia2, title: 'Grand Canyon, USA', alt: 'Picture taken in Grand Canyon, USA' },
+  { img: 1火车, title: '回老家, 昆明', alt: 'Picture taken in 回老家, 昆明' },
+  { img: 2勐麻河, title: '钻箐沟, 临沧', alt: 'Picture taken in 钻箐沟, 临沧' },
+  { img: 3勐麻河交汇, title: '澜沧江支流码头, 临沧', alt: 'Picture taken in 澜沧江支流码头, 临沧' },
+  { img: 4树上采摘, title: '大姐爬树采单株, 临沧', alt: 'Picture taken in 大姐爬树采单株, 临沧' },
+  { img: 5白花, title: '路边野花, 临沧', alt: 'Picture taken in 路边野花, 临沧' },
+  { img: 6吊在茶树上, title: '2021年在勐麻河, 临沧', alt: 'Picture taken in 2021年在勐麻河, 临沧' },
+  { img: 7勐麻河土基房, title: '村子里绕绕, 临沧', alt: 'Picture taken in 村子里绕绕, 临沧' },
+  { img: 8在勐麻河竹林, title: '去茶地经过竹林, 临沧', alt: 'Picture taken in 去茶地经过竹林, 临沧' },
+  { img: 9茶树和我, title: '经典摆拍, 临沧', alt: 'Picture taken in 经典摆拍, 临沧' },
+  { img: 10河边休息, title: '在河边休息下, 临沧', alt: 'Picture taken in 在河边休息下, 临沧' },
 ];
