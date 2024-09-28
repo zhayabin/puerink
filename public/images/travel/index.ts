ed0180a24504af31a667c111bc8c39a832eeb092
zhayabin <80046768+zhayabin@users.noreply.github.com>
2024-09-29 02:36:02 +0800

    Update index.ts

--- public/images/travel/index.ts
+++ public/images/travel/index.ts
@@ -9,14 +9,14 @@ import 茶树和我 from './9茶树和我.jpg';
 import 河边休息 from './10河边休息.png';
 
 export const travelImages = [
-  { img: 1火车, title: '回老家, 昆明', alt: 'Picture taken in 回老家, 昆明' },
-  { img: 2勐麻河, title: '钻箐沟, 临沧', alt: 'Picture taken in 钻箐沟, 临沧' },
-  { img: 3勐麻河交汇, title: '澜沧江支流码头, 临沧', alt: 'Picture taken in 澜沧江支流码头, 临沧' },
-  { img: 4树上采摘, title: '大姐爬树采单株, 临沧', alt: 'Picture taken in 大姐爬树采单株, 临沧' },
-  { img: 5白花, title: '路边野花, 临沧', alt: 'Picture taken in 路边野花, 临沧' },
-  { img: 6吊在茶树上, title: '2021年在勐麻河, 临沧', alt: 'Picture taken in 2021年在勐麻河, 临沧' },
-  { img: 7勐麻河土基房, title: '村子里绕绕, 临沧', alt: 'Picture taken in 村子里绕绕, 临沧' },
-  { img: 8在勐麻河竹林, title: '去茶地经过竹林, 临沧', alt: 'Picture taken in 去茶地经过竹林, 临沧' },
-  { img: 9茶树和我, title: '经典摆拍, 临沧', alt: 'Picture taken in 经典摆拍, 临沧' },
-  { img: 10河边休息, title: '在河边休息下, 临沧', alt: 'Picture taken in 在河边休息下, 临沧' },
+  { img: 火车, title: '回老家, 昆明', alt: 'Picture taken in 回老家, 昆明' },
+  { img: 勐麻河, title: '钻箐沟, 临沧', alt: 'Picture taken in 钻箐沟, 临沧' },
+  { img: 勐麻河交汇, title: '澜沧江支流码头, 临沧', alt: 'Picture taken in 澜沧江支流码头, 临沧' },
+  { img: 树上采摘, title: '大姐爬树采单株, 临沧', alt: 'Picture taken in 大姐爬树采单株, 临沧' },
+  { img: 白花, title: '路边野花, 临沧', alt: 'Picture taken in 路边野花, 临沧' },
+  { img: 吊在茶树上, title: '2021年在勐麻河, 临沧', alt: 'Picture taken in 2021年在勐麻河, 临沧' },
+  { img: 勐麻河土基房, title: '村子里绕绕, 临沧', alt: 'Picture taken in 村子里绕绕, 临沧' },
+  { img: 在勐麻河竹林, title: '去茶地经过竹林, 临沧', alt: 'Picture taken in 去茶地经过竹林, 临沧' },
+  { img: 茶树和我, title: '经典摆拍, 临沧', alt: 'Picture taken in 经典摆拍, 临沧' },
+  { img: 河边休息, title: '在河边休息下, 临沧', alt: 'Picture taken in 在河边休息下, 临沧' },
 ];
