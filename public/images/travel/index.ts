8e3e60539b86f3db589faf4801728eeb9354f695
zhayabin <zhayabin@icloud.com>
2024-10-12 14:28:23 +0800

    first commit

--- public/images/travel/index.ts
+++ public/images/travel/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 冰岛老寨 from './冰岛老寨.jpg';
 import 冰岛老寨2 from './冰岛老寨2.jpg';
 import 采茶归来 from './采茶归来.jpg';
@@ -53,29 +52,4 @@ export const travelImages = [
   { img: 小勐峨看双江, title: '小勐峨看双江' },
   { img: 叶底审评, title: '叶底审评' },
   { img: 叶底审评2, title: '叶底审评' },
-
-=======
-import 火车 from './1火车.jpg';
-import 勐麻河 from './2勐麻河.jpg';
-import 勐麻河交汇 from './3勐麻河交汇.jpg';
-import 树上采摘 from './4树上采摘.jpg';
-import 白花 from './5白花.jpg';
-import 吊在茶树上 from './6吊在茶树上.jpg';
-import 勐麻河土基房 from './7勐麻河土基房.jpg';
-import 在勐麻河竹林 from './8在勐麻河竹林.jpg';
-import 茶树和我 from './9茶树和我.jpg';
-import 河边休息 from './10河边休息.png';
-
-export const travelImages = [
-  { img: 火车, title: '回老家, 昆明', alt: 'Picture taken in 回老家, 昆明' },
-  { img: 勐麻河, title: '钻箐沟, 临沧', alt: 'Picture taken in 钻箐沟, 临沧' },
-  { img: 勐麻河交汇, title: '澜沧江支流码头, 临沧', alt: 'Picture taken in 澜沧江支流码头, 临沧' },
-  { img: 树上采摘, title: '大姐爬树采单株, 临沧', alt: 'Picture taken in 大姐爬树采单株, 临沧' },
-  { img: 白花, title: '路边野花, 临沧', alt: 'Picture taken in 路边野花, 临沧' },
-  { img: 吊在茶树上, title: '2021年在勐麻河, 临沧', alt: 'Picture taken in 2021年在勐麻河, 临沧' },
-  { img: 勐麻河土基房, title: '村子里绕绕, 临沧', alt: 'Picture taken in 村子里绕绕, 临沧' },
-  { img: 在勐麻河竹林, title: '去茶地经过竹林, 临沧', alt: 'Picture taken in 去茶地经过竹林, 临沧' },
-  { img: 茶树和我, title: '经典摆拍, 临沧', alt: 'Picture taken in 经典摆拍, 临沧' },
-  { img: 河边休息, title: '在河边休息下, 临沧', alt: 'Picture taken in 在河边休息下, 临沧' },
->>>>>>> 5100fde1988a2d6cb352315c53e872b012a0ecc0
-];
+]
